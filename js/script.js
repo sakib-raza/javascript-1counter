abe61920b7d777321a64bc87e4838c5026b7c2d7
saqib rizvi <rzsakib99@gmail.com>
2026-05-05 13:51:15 +0530

    counter final

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
- 
- 
+const countEl = document.querySelector(".count");
+const addEl = document.querySelector(".plus-btn");
+const subEl = document.querySelector(".minus-btn");
+const changeByEl = document.querySelector(".changeby");
+const resetEl = document.querySelector(".reset-btn");
+
+addEl.addEventListener("click", ()=>{
+    const counText = parseInt(countEl.innerText);
+    const changeByValue = parseInt(changeByEl.value);
+    countEl.innerText = counText+changeByValue;
+})
+subEl.addEventListener("click", ()=>{
+    const counText = parseInt(countEl.innerText);
+    const changeByValue = parseInt(changeByEl.value);
+    countEl.innerText = counText-changeByValue;
+})
+resetEl.addEventListener("click", ()=>{
+    countEl.innerText = "0";
+})
